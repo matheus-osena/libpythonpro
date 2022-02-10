d3142c0de0d2b7b1ff69fdece47d6e785dd26220
matheus <matheus_osena@hotmail.com>
2022-02-10 11:17:55 -0300

    Criada a função buscar_avatar que busca informações da api do github e retorna um link com a imagem de usuário utilizada pelo nome do usuário utilizado como parâmetro.

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,15 @@
+import requests as requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usúario no Github
+    :param usuario:
+    :return: str com o link do avatar
+    """
+    url = f"https://api.github.com/users/{usuario}"
+    resp = requests.get(url)
+    return resp.json()["avatar_url"]
+
+if __name__ == "__main__":
+    print(buscar_avatar("matheus-osena"))
